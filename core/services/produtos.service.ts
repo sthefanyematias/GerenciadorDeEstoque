df963c0302a07c03763061e4275dfe39794ee23c
Sthefany Evangelista Matias <117083316+sthefanyematias@users.noreply.github.com>
2025-12-13 09:41:29 -0300

    Change API endpoint to local path

--- core/services/produtos.service.ts
+++ core/services/produtos.service.ts
@@ -8,7 +8,8 @@ import { Observable, throwError, catchError } from 'rxjs';
 })
 export class ProdutosService {
   //private readonly API = 'http://localhost:3000/estoque';
-  private readonly API = 'https://my-json-server.typicode.com/sthefanyematias/GerenciadorDeEstoque/estoque'; 
+  //private readonly API = 'https://my-json-server.typicode.com/sthefanyematias/GerenciadorDeEstoque/estoque'; 
+  private readonly API = '/estoque';
 
   constructor(private http: HttpClient) { }
 
